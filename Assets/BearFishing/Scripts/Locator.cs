a234b8377a77137735c8ba97dcf050a98b06d717
penguin-del <jrhut1355@gmail.com>
2024-03-26 22:16:06 -0400

    Bear Fishing Progress
    Fish are moving; target is controllable by player character; spear is created though it is not integrated into the game

--- Assets/BearFishing/Scripts/Locator.cs
+++ Assets/BearFishing/Scripts/Locator.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Locator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    static public Locator S { get; private set; }  // Singleton property    // a
+
+    [Header("Inscribed")]
+    // These fields control the movement of the ship
+    public float speed = 30;
+
+    void Awake()
     {
-        
+        if (S == null)
+        {
+            S = this; // Set the Singleton only if it’s null                  // c
+        }
+        else
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Locator.S!");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Pull in information from the Input class
+        float hAxis = Input.GetAxis("Horizontal");                            // d
+        float vAxis = Input.GetAxis("Vertical");                              // d
+
+        // Change transform.position based on the axes
+        Vector3 pos = transform.position;
+        pos.x += hAxis * speed * Time.deltaTime;
+        pos.y += vAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //FireHarpoon();
+        }
     }
+
+    //void FireHarpoon()
+    //{
+    //    GameObject go = Instantiate<GameObject> spear;
+    //     = new Vector3(0,3,0);
+
+  
+    //}
 }
