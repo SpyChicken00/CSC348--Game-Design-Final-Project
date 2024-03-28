cdf219670fd9f29cf8716cf0429425a83122fb0d
HayesFBrown <hayesfbrown@gmail.com>
2024-03-28 10:06:40 -0400

    Started work on Transitions

---
+++ Assets/Hub/Scripts/Transition.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour
+{
+    public Animator animator;
+    public float transitionDelayTime = 1.0f;
+
+    void Awake()
+    {
+        animator = GameObject.Find("Transition").GetComponent<Animator>();
+    }
+
+    public void LoadLevel(string newLevel)
+    {
+        StartCoroutine(DelayLoadLevel(newLevel));
+    }
+
+    IEnumerator DelayLoadLevel(string newLevel)
+    {
+        animator.SetTrigger("TriggerTransition");
+        yield return new WaitForSeconds(transitionDelayTime);
+        SceneManager.LoadScene(newLevel);
+    }
+}
