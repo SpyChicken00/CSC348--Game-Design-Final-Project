1988e119c32085bcaeb5f1741e321fb3cf216c97
Jace Rettig <jace.rettig@furman.edu>
2024-04-01 21:50:04 -0400

    BearClimbing Progress
    Added bear movement and ability to eat branches. Still need to implement bear line of sight and winning/losing mechanics

--- Assets/BearBambooClimbing/Scripts/BearAI.cs
+++ Assets/BearBambooClimbing/Scripts/BearAI.cs
@@ -4,15 +4,90 @@ using UnityEngine;
 
 public class BearAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private enum Movement { Up, Down, Wait }
+    public enum Side { Left, Right }
+    private Movement currentMovement = Movement.Wait;
+    private int timer = 0;
+    //private Side defaultSide;
+    
+    
+    [SerializeField]
+    public Side currentSide;
+    public float movementSpeed = 0.01f;
+
+    void Start() {
+        //defaultSide = currentSide;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer >= 120) {
+            ChooseRandomMovement();
+            ChooseSide();
+            timer = 0;
+        }
+        else {
+            timer += 1;
+        }
+
+        switch (currentMovement) {
+            case Movement.Up:
+                if (transform.position.y < 4) {transform.position += new Vector3(0, movementSpeed, 0);}
+                break;
+            case Movement.Down:
+                if (transform.position.y > -4.3) {transform.position += new Vector3(0, -movementSpeed, 0);}
+                break;
+            case Movement.Wait:
+                break;
+        }
+  }
+
+    private void ChooseRandomMovement() {
+        // Randomly move up or down
+        int num = Random.Range(0, 3);
+        switch (num) {
+            case 0:
+                currentMovement = Movement.Up;
+                break;
+            case 1:
+                currentMovement = Movement.Down;
+                break;
+            case 2:
+                currentMovement = Movement.Wait;
+                break;
+        }
     }
+
+    private void ChooseSide() {
+        // Randomly move to the left or right
+        int num = Random.Range(0, 10);
+        switch (num) {
+            case 0:
+                if (currentSide != Side.Left) {
+                    transform.position = new Vector3(transform.position.x-2, transform.position.y, 0);
+                    currentSide = Side.Left;
+                }
+                break;
+            case 1:
+                if (currentSide != Side.Right) {
+                    transform.position = new Vector3(transform.position.x+2, transform.position.y, 0);
+                    currentSide = Side.Right;
+                }
+                break;
+            default:
+                //currentSide = defaultSide;
+                break;
+        }
+    
+    }
+    //TODO check for collision with line of sight and player
+    private void enterLineOfSight() {
+        //if player is in line of sight, start countdown to attack 
+    }
+
+    //bears move up and down their trees at random intervals
+    //be able to adjust speed and frequency of movement for difficulty 
+    //bears can switch sides of tree?
+    //bears can eat berries?
 }
--- Assets/BearBambooClimbing/Scripts/Player.cs
+++ Assets/BearBambooClimbing/Scripts/Player.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //GameObject player;
+    private GameObject head;
+    private bool keyDown;
+    private int holdCounter = 0;
+    public int holdFrames = 100;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject player = GameObject.Find("Player");
+        head = player.transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
@@ -18,13 +23,13 @@ public class Player : MonoBehaviour
         if(Input.GetKey(KeyCode.W))
         {
             //move player up
-            if (transform.position.y < 4) {transform.position += new Vector3(0, 0.01f, 0);}
+            if (transform.position.y < 4) {transform.position += new Vector3(0, 0.015f, 0);}
             //4
         }
         if (Input.GetKey(KeyCode.S))
         {
             //move player down
-            if (transform.position.y > -4.3) {transform.position += new Vector3(0, -0.01f, 0);}
+            if (transform.position.y > -4.3) {transform.position += new Vector3(0, -0.015f, 0);}
             //-4.3
         }
         if (Input.GetKey(KeyCode.A))
@@ -39,5 +44,63 @@ public class Player : MonoBehaviour
             float y = transform.position.y;
             transform.position = new Vector3(1, y, 0);
         }
+
+        keyDown = Input.GetKey(KeyCode.Space);
+
+        if (keyDown) {holdCounter++;}
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            holdCounter = 0;
+        }
+        
+            //TODO how to decrease durabiliy with brief pause but enable holding button to continuously progress
+        
+
+        //check for branch collision and space keydown
+        //if so, decrease berry durability, play sound
     }
+
+    
+    
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        //Debug.Log("Colliding with branch");
+        //if(keyDown && collider.compareTag("Branch"))
+        if(keyDown && holdCounter > holdFrames)
+        {
+            //Debug.Log("Space key pressed");
+            //decrease berry durability
+            collider.gameObject.GetComponent<Branch>().DecreaseBerryDurability();
+            holdCounter = 0;
+            //GetComponent..gameObject.GetComponent<Branch>().DecreaseBerryDurability();
+            //StartCoroutine(waitForSeconds(3));
+            //TODO how to decrease durabiliy with brief pause but enable holding button to continuously progress
+        }
+
+
+
+
+
+
+        //allow player to pass through branch
+        //Physics2D.IgnoreCollision(collider, GetComponent<Collider2D>());
+        //if (collider.gameObject.tag == "Branch")
+        //{
+            //Debug.Log("Colliding with branch");
+            //head.GetComponent<Head>().isColliding = true;
+            //head.GetComponent<Head>().collidingBranch = collider.gameObject;
+        //
+    }
+
+
+
+
+
+
+
 }
+    
+
+
+
