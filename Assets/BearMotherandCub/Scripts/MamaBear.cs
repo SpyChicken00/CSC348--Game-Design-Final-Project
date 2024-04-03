701ff79522e0f395ac87eccd7537898d9f71bdf6
penguin-del <jrhut1355@gmail.com>
2024-04-03 09:56:57 -0400

    Mother Cub work

--- Assets/BearMotherandCub/Scripts/MamaBear.cs
+++ Assets/BearMotherandCub/Scripts/MamaBear.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class MamaBear : MonoBehaviour
 {
+    public int numChildren;
+    public GameObject baby;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenerateBaby(numChildren);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void GenerateBaby(int children)
+    {
+        for(int i = 0; i < numChildren; i++)
+        {
+            Instantiate<GameObject>(baby);
+        }
     }
 }
