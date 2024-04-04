2b66b8e123b1f79dda363dbb2fce31e00742e10f
penguin-del <jrhut1355@gmail.com>
2024-04-04 09:23:22 -0400

    Fish Game fully functioning

--- Assets/Minigames/BearMotherandCub/Scripts/MainCharacter.cs
+++ Assets/Minigames/BearMotherandCub/Scripts/MainCharacter.cs
@@ -1,23 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class MainCharacter : MonoBehaviour
 {
     public float speed = 2.5f;
+    public GameObject startPnt;
+    public GameObject endPnt;
 
-    // Update is called once per frame
+    void Start()
+    {
+        this.transform.position = startPnt.transform.position;
+    }
+
     void Update()
     {
-        // Pull in information from the Input class
-        float hAxis = Input.GetAxis("Horizontal");                            // d
-        float vAxis = Input.GetAxis("Vertical");                              // d
+            // Pull in information from the Input class
+            float hAxis = Input.GetAxis("Horizontal");                            // d
+            float vAxis = Input.GetAxis("Vertical");                              // d
 
-        // Change transform.position based on the axes
-        Vector3 pos = transform.position;
-        pos.x += hAxis * speed * Time.deltaTime;
-        pos.y += vAxis * speed * Time.deltaTime;
-        transform.position = pos;
+            // Change transform.position based on the axes
+            Vector3 pos = transform.position;
+            pos.x += hAxis * speed * Time.deltaTime;
+            pos.y += vAxis * speed * Time.deltaTime;
+            transform.position = pos;
+    }
+
+    public void Restart()
+    {
+        this.transform.position = startPnt.transform.position;
     }
 }
