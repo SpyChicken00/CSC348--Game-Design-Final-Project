9cd5c86c9dbc9d1d68d8978ee2bc1232683e5506
penguin-del <jrhut1355@gmail.com>
2024-04-16 05:17:32 -0400

    Housekeeping

--- Assets/Minigames/Hub - Title Screen/Scripts/DialogueBox.cs
+++ Assets/Minigames/Hub - Title Screen/Scripts/DialogueBox.cs
@@ -83,41 +83,4 @@ public class DialogueBox: MonoBehaviour
         yield return new WaitForSeconds(wait);
         SceneManager.LoadScene("Hub");
     }
-
-    //public void StartDialogue(string[] dialogue, int startPosition, string name)
-    //{
-    //    nameText.text = name + "...";
-    //    dialogueBox.gameObject.SetActive(true);
-    //    StopAllCoroutines();
-    //    StartCoroutine(RunDialogue(dialogue, startPosition));
-    //}
-
-    //IEnumerator RunDialogue(string[] dialogue, int startPosition)
-    //{
-    //    skipLineTriggered = false;
-    //    OnDialogueStarted?.Invoke();
-
-    //    for (int i = startPosition; i < dialogue.Length; i++)
-    //    {
-    //        dialogueText.text = dialogue[i];
-    //        while (skipLineTriggered == false)
-    //        {
-    //            // Wait for the current line to be skipped
-    //            yield return null;
-    //        }
-    //        skipLineTriggered = false;
-    //    }
-
-    //    OnDialogueEnded?.Invoke();
-    //    dialogueBox.gameObject.SetActive(false);
-    //}
-
-    //public void SkipLine()
-    //{
-    //    skipLineTriggered = true;
-    //}
-
-
-
-
 }
