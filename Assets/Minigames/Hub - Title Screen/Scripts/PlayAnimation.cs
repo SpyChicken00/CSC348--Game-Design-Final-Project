4f1cb24208ca8e7f1da667024a33c4ad03ed017e
penguin-del <jrhut1355@gmail.com>
2024-04-18 09:13:29 -0400

    Start Screen Implemented

---
+++ Assets/Minigames/Hub - Title Screen/Scripts/PlayAnimation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayAnimation : MonoBehaviour
+{
+
+    public AnimationClip walk;
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("Active");
+            StartCoroutine(BeginCutscene(1f));
+        }
+    }
+
+    IEnumerator BeginCutscene(float wait)
+    {
+        yield return new WaitForSeconds(wait);
+        SceneManager.LoadScene("Opening");
+    }
+}
